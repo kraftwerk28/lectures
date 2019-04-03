a581de9f7c1e6286847996e6c391d274599fa992
kraftwerk28 <kefirchik3@gmail.com>
2019-04-03 18:20:25 +0300

    Chore: refactor proj structure

---
+++ 02-telegram-bot-api/lect-bot2/bot.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(
+  '762563466:AAGRCXFvLoNOEVUrPSN-Zn2e4ggLJYDHL8o',
+  { polling: true })
+
+bot.on('text', (message) => {
+  bot.sendMessage(message.chat.id, 'Hello!')
+})
+
+bot.onText(/\/push/, (message, match) => {
+  bot.sendSticker(message.chat.id, 'CAADAgADMgADKIxPDen6wQyFw6RcAg')
+})
